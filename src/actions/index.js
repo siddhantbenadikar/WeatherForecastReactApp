6662c99b2fa452d2e3b62f0a832ae144b2e3445b
siddhantbenadikar <siddhantn@hotmail.com>
2018-04-16 17:19:19 -0400

    added weather_list

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '100a9ed26483fb8ad08d85ecd9874267';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
